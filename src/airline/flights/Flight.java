745314193ed3bd224a551c5d9b16b8294698189e
gustavolessa1 <gustavolessa1@10.200.9.126>
2018-03-26 17:21:31 +0100

    Flight class creation

--- src/airline/flights/Flight.java
+++ src/airline/flights/Flight.java
@@ -5,10 +5,23 @@
  */
 package airline.flights;
 
+import airline.aircrafts.Aircraft;
+
 /**
  *
- * @author gustavolessa1
+ * @author gustavolessa
  */
 public class Flight {
+    private int id;
+    private int lastId;
+    private String origin;
+    private String destination;
+    private String departureTime;
+    private String arrivalTime;
+    private String dateOfFlight;
+    private String dateOfArrival;
+    private Aircraft aircraftAssigned;
+    
+    
     
 }
