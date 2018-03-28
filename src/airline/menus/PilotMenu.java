4691d0a86b380adb2a8e858cb7457fd07ebf01f5
rbsrafa <rbsrafa@gmail.com>
2018-03-28 15:26:41 +0100

    Flight screen info changed. Menu validation changed

--- src/airline/menus/PilotMenu.java
+++ src/airline/menus/PilotMenu.java
@@ -3,6 +3,7 @@ package airline.menus;
 
 import airline.employees.Pilot;
 import airline.data.Data;
+import airline.employees.Rating;
 
 
 /**
@@ -30,9 +31,12 @@ public class PilotMenu extends Menu{
                 this.searchPilot();
                 break;
             case 3:
-                this.returnToMainMenu();
+                this.addPilot();
                 break;
             case 4:
+                this.returnToMainMenu();
+                break;
+            case 5:
                 this.exitProgram();
                 break;
             default:
@@ -44,16 +48,41 @@ public class PilotMenu extends Menu{
     @Override
     public String toString(){
         return
-            "+--------------------------+\n" +
+            "\n+--------------------------+\n" +
             "| Pilots Menu              |\n" + 
             "+--------------------------+\n" +
-            "| Please select an option: |\n" +
-            "|                          |\n" +
             "| 1 - Show Pilots list     |\n" +
             "| 2 - Find Pilot by id     |\n" +
-            "| 3 - Return to Main Menu  |\n" + 
-            "| 4 - Exit Program         |\n" + 
-            "+--------------------------+\n";
+            "| 3 - Add new Pilot        |\n" +    
+            "| 4 - Return to Main Menu  |\n" + 
+            "| 5 - Exit Program         |\n" + 
+            "+--------------------------+\n" +
+            "Please select an option:   ";
+    }
+    
+    public void addPilot(){
+        Pilot p = null;
+        input.nextLine();        
+        System.out.println("Please type the pilot name:");
+        String name = input.nextLine();
+        System.out.println("Please type the pilot Rating:\nAvailable ratings: ");
+        for(Rating r: Rating.values()){
+            System.out.println(r+" (Max number of passengers: " + r.getPassengers() + ")");
+        }
+        String rating = input.nextLine();
+        
+        if(rating.startsWith("A") || rating.startsWith("a")){
+            p = new Pilot(name, Rating.A);
+        }else if(rating.startsWith("B") || rating.startsWith("b")){
+            p = new Pilot(name, Rating.B);
+        }else if(rating.startsWith("C") || rating.startsWith("c")){
+            p = new Pilot(name, Rating.C);
+        }else if(rating.startsWith("D") || rating.startsWith("d")){
+            p = new Pilot(name, Rating.D);
+        }else{
+            System.out.println("sth wrong");
+        }
+        super.data.getPilots().add(p);
     }
 
     public void displayPilots(){
